67f46528c81092a92a8375586414a3d7dc75e4d4
Mohammad <m@mohammadi.io>
2022-08-15 12:06:57 +0300

    Moving routers to the main

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
-import uvicorn
+from blog import models
+from blog.database import engine
+from blog.routers import authentication, blog, user
 
+models.Base.metadata.create_all(bind=engine)  # creates all database tables
 app = FastAPI()
+app.include_router(router=authentication.router)
+app.include_router(router=blog.router)
+app.include_router(router=user.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):  # function variable name must be like in the path, str is the default for variable type
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/blog")
-def blog(limit: int = 10, published: bool = True):  # if we don't provide the parameter we get an error
-    if published:
-        return {'message': f'{limit} number of blog from DB'}
-    return {'message': 'not published'}
-
-
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool]
-    # published: bool = None # equal to above, the difference is just in editors
-    # published: bool = True
-
-
-@app.post('/blog')
-def create_blog(blog: Blog):
-    return f'The blog with title as {blog.title} is created!'
-
-
-# if __name__ == '__main__': # to change the port of running application, you must run main.py for this
-    # uvicorn.run(app=app, host='127.0.0.1', port=9000)
-    # uvicorn.run(app=app, port=9000)
