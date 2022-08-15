950c34826d48723a1ade1b871e37696d25bef36e
Mohammad <m@mohammadi.io>
2022-08-15 12:26:56 +0300

    editing Procfile

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from blog import models
 from blog.database import engine
 from blog.routers import authentication, blog, user
 
-models.Base.metadata.create_all(bind=engine)  # creates all database tables
+# models.Base.metadata.create_all(bind=engine)  # creates all database tables
 app = FastAPI()
-app.include_router(router=authentication.router)
-app.include_router(router=blog.router)
-app.include_router(router=user.router)
+# app.include_router(router=authentication.router)
+# app.include_router(router=blog.router)
+# app.include_router(router=user.router)
 
 
 @app.get("/")
